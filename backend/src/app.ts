f381beca942cb10cca0b401b84c9e808eda50d96
root <root@development.local>
2023-05-20 08:34:04 +0000

    first commit

---
+++ backend/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+
+let app = express();
+
+app.use(express.static('public'));
+
+function requireAuth(req: express.Request, res: express.Response, next: express.NextFunction) {
+    // TODO: implement authorization logic
+    if (req.headers && req.headers.authorization) {
+        next();
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+}
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+export function start() {
+    app.listen(3000, () => {
+        console.log("Server running on port 3000");
+    });
+}
